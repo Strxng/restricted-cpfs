41242043c7210dbd69d1704f2d7740eef645d4e0
Leo B <leonardo_picinin02@hotmail.com>
2023-08-14 15:19:38 -0300

    adjust data source name

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { DataSource, DataSourceOptions } from "typeorm";
 
-export const AppDataSource = new DataSource({
+export const databaseConnection = new DataSource({
 	type: process.env.TYPEORM_CONNECTION,
 	url: process.env.TYPEORM_URL,
 	extra: { ssl: true },
--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,15 @@ import "dotenv/config";
 
 import express from "express";
 import cors from "cors";
-import { AppDataSource } from "./database/connection";
+import { databaseConnection } from "./database/connection";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-AppDataSource.initialize()
+databaseConnection
+	.initialize()
 	.then(() => {
 		app.listen(8080, () => console.log("server runing"));
 	})
